ecc8c711d5cd71843fcd61a92b0d2e806c5d7ecf
Melvin Macaranas <greatwinblowaway@gmail.com>
2021-02-08 16:30:01 +0700

    create mlendpoint

--- ml-endpoint/src/index.ts
+++ ml-endpoint/src/index.ts
@@ -1,38 +1,163 @@
-/**
- * TODO(developer): Uncomment these variables before running the sample.
- */
-const SUBSCRIPTION_NAME = 'YOUR_SUBSCRIPTION_NAME';
-const TIMEOUT = 60;
+require('dotenv').config();
+const {v1} = require('@google-cloud/pubsub');
+import axios from 'axios';
+import {updateActualResultByImageId} from './sql';
+import {getImageFromStorage} from './storage';
 
-// Imports the Google Cloud client library
-const {PubSub} = require('@google-cloud/pubsub');
+const projectId = process.env.PROJECT_ID;
+const subscriptionName = process.env.PUBSUB_SUBSCRIPTION_NAME;
 
-// Creates a client; cache this for further use
-const pubSubClient = new PubSub();
+// Creates a client; cache this for further use.
+const subClient = new v1.SubscriberClient({
+  keyFilename: 'serviceAccountKey.json',
+});
 
-function listenForMessages() {
-  // References an existing subscription
-  const subscription = pubSubClient.subscription(SUBSCRIPTION_NAME);
+async function synchronousPull() {
+  const formattedSubscription = subClient.subscriptionPath(
+    projectId,
+    subscriptionName
+  );
 
-  // Create an event handler to handle messages
-  let messageCount = 0;
-  const messageHandler = message => {
-    console.log(`Received message ${message.id}:`);
-    console.log(`\tData: ${message.data}`);
-    console.log(`\tAttributes: ${message.attributes}`);
-    messageCount += 1;
-
-    // "Ack" (acknowledge receipt of) the message
-    message.ack();
+  // The maximum number of messages returned for this request.
+  // Pub/Sub may return fewer than the number specified.
+  const request = {
+    subscription: formattedSubscription,
+    maxMessages: 1,
   };
 
-  // Listen for new messages until timeout is hit
-  subscription.on('message', messageHandler);
+  // The subscriber pulls a specified number of messages.
+  const [response] = await subClient.pull(request);
+  console.log('response length', response.receivedMessages.length);
 
-  setTimeout(() => {
-    subscription.removeListener('message', messageHandler);
-    console.log(`${messageCount} message(s) received.`);
-  }, TIMEOUT * 1000);
+  if (response.receivedMessages.length <= 0) {
+    return [];
+  }
+
+  // Process the messages.
+  const ackIds = [];
+  const messages: {
+    imageId: string;
+    fileName: string;
+  }[] = [];
+  for (const message of response.receivedMessages) {
+    console.log(`Received message: ${message.message.data}`);
+    ackIds.push(message.ackId);
+    messages.push(JSON.parse(message.message.data));
+  }
+
+  if (ackIds.length !== 0) {
+    // Acknowledge all of the messages. You could also ackknowledge
+    // these individually, but this is more efficient.
+    const ackRequest = {
+      subscription: formattedSubscription,
+      ackIds: ackIds,
+    };
+
+    await subClient.acknowledge(ackRequest);
+  }
+
+  console.log('Done.');
+  return messages;
 }
 
-listenForMessages();
+async function predictImage(imageId: string, fileName: string) {
+  console.log('predicting image...');
+  console.log('imageId: ', imageId);
+  console.log('fileName:', fileName);
+
+  console.log('Getting image from URL...');
+  // const response = await axios.get(imageUrl);
+  // const image: Buffer = response.data;
+  const image: Buffer = await getImageFromStorage(fileName);
+  console.log('Get image done.');
+
+  console.log('Sending image to model...');
+  let modelResponse: any;
+  try {
+    modelResponse = await axios.post(process.env.MODEL_URL || '', {
+      instances: [
+        {
+          image_bytes: {
+            b64: image.toString('base64'),
+          },
+          key: imageId,
+        },
+      ],
+    });
+    console.log('Send image to model done.');
+  } catch (error) {
+    console.error('something is wrong while sending image to model');
+    console.error(error);
+  }
+
+  if (
+    !modelResponse ||
+    !modelResponse.data ||
+    !modelResponse.data.predictions ||
+    modelResponse.data.predictions.length <= 0 ||
+    !modelResponse.data.predictions[0].scores ||
+    modelResponse.data.predictions[0].scores.length <= 0
+  ) {
+    console.log('something is wrong with modelResponse', modelResponse.data);
+    console.log(modelResponse.data.predictions[0]);
+  }
+
+  console.log(modelResponse.data.predictions[0]);
+  // console.log(modelResponse.data.predictions[0].scores);
+  // console.log(modelResponse.data.predictions[0].labels);
+
+  let banProb: number;
+
+  if (modelResponse.data.predictions[0].labels[0] === 'ban') {
+    banProb = modelResponse.data.predictions[0].scores[0];
+  } else {
+    banProb = modelResponse.data.predictions[0].scores[1];
+  }
+
+  let predictionResult: string;
+
+  // classify by the probability
+  if (banProb >= 0.9) {
+    predictionResult = 'BAN';
+  } else if (banProb < 0.9 && banProb >= 0.6) {
+    predictionResult = 'UNCERTAIN';
+  } else {
+    predictionResult = 'PASS';
+  }
+
+  // update result to DB
+  await updateActualResultByImageId(imageId, predictionResult);
+}
+
+async function main() {
+  console.log('Server in running...');
+  let isPaused = false;
+  setInterval(async () => {
+    if (isPaused) {
+      return;
+    }
+    isPaused = true;
+
+    try {
+      const messages = await synchronousPull();
+
+      if (messages.length <= 0) {
+        // NO MESSAGE IN THE QUEUE
+        console.log('No message received');
+      } else {
+        // HAVE MESSAGE
+        console.log('have message');
+        for (const message of messages) {
+          await predictImage(message.imageId, message.fileName);
+          console.log(message);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    isPaused = false;
+  }, 5000);
+}
+
+main().catch(console.error);
+// synchronousPull().catch(console.error);
---
+++ ml-endpoint/src/sql.ts
@@ -0,0 +1,55 @@
+import {OkPacket, escape, ConnectionConfig, createConnection} from 'mysql';
+import * as util from 'util';
+
+// when deploy, change from "host" to "socketPath"
+
+const config: ConnectionConfig = {
+  socketPath: process.env.SQL_HOST,
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PASS,
+  database: process.env.SQL_DATABASE,
+  charset: 'utf8',
+  timezone: 'utc',
+};
+
+const makeDb = (config: ConnectionConfig) => {
+  try {
+    const connection = createConnection(config);
+
+    return {
+      query(queryString: string) {
+        return util.promisify(connection.query).call(connection, queryString);
+      },
+      close() {
+        return util.promisify(connection.end).call(connection);
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error occur while connecting to DB.');
+  }
+};
+
+const db = makeDb(config);
+
+const updateActualResultByImageId = async (
+  id: string,
+  predictedResult: string
+): Promise<void> => {
+  try {
+    const queryString = `UPDATE validation_result SET predicted_result = ${escape(
+      predictedResult
+    )}, actual_result = ${escape(predictedResult)}
+    WHERE id = ${escape(id)};`;
+    const query = db.query(queryString);
+    const result = <OkPacket>await query;
+    if (!result.affectedRows || result.affectedRows < 1) {
+      throw new Error();
+    }
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error occur while updating prediction result.');
+  }
+};
+
+export {updateActualResultByImageId};
