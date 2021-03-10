8ac06624652db844056c019a5618bebad4335d70
Melvin Macaranas <melvin@nsquared.asia>
2021-03-10 16:17:39 +0700

    increase timeout to 10s

--- frontend/src/api/httpClient.ts
+++ frontend/src/api/httpClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const httpClient = axios.create({
   baseURL: "https://asia-southeast2-poc-innovation-iot.cloudfunctions.net",
-  timeout: 2500,
+  timeout: 10000,
 });
 
 export default httpClient;
